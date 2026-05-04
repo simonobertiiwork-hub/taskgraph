57107989bcc33566baf25a68be6cd0cf5f1a32fe
Simon Obertii <simon.obertii2000@gmail.com>
2026-05-04 22:58:49 +0300

    refactor: migrate to async SQLAlchemy and configure DB pool

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import asyncpg
 from fastapi import Depends, FastAPI, HTTPException
-from sqlalchemy.orm import Session
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.config import settings
 from app.db.session import get_db
@@ -27,35 +28,39 @@ async def db_check():
 
 
 @app.get("/tasks")
-def get_tasks(db: Session = Depends(get_db)):
+async def get_tasks(db: AsyncSession = Depends(get_db)):
     """Вернуть все задачи из БД"""
-    tasks = db.query(Task).all()
+    result = await db.execute(select(Task))
+    tasks = result.scalars().all()  # запрос → список задач
     return tasks
 
 
 @app.post("/tasks")
-def create_task(task_data: TaskCreate, db: Session = Depends(get_db)):
+async def create_task(task_data: TaskCreate, db: AsyncSession = Depends(get_db)):
     """Создать новую задачу (статус new проставится автоматически)."""
     task = Task(title=task_data.title)
     db.add(task)
-    db.commit()
-    db.refresh(task)  # подтягиваем id после вставки
+    await db.commit()
+    await db.refresh(task)  # подтягиваем id после вставки
     return task
 
 
 @app.get("/tasks/{task_id}")
-def get_task(task_id: int, db: Session = Depends(get_db)):
+async def get_task(task_id: int, db: AsyncSession = Depends(get_db)):
     """Вернуть одну задачу по id"""
-    task = db.query(Task).filter(Task.id == task_id).first()
+    result = await db.execute(select(Task).where(Task.id == task_id))
+    task = result.scalar_one_or_none()  # запрос → одна задача или None
     if not task:
         raise HTTPException(status_code=404, detail="Task not found")
     return task
 
 
 @app.patch("/tasks/{task_id}")
-def update_task(task_id: int, task_data: TaskUpdate, db: Session = Depends(get_db)):
+async def update_task(task_id: int, task_data: TaskUpdate, db: AsyncSession = Depends(get_db)):
+    result = await db.execute(select(Task).where(Task.id == task_id))
+    task = result.scalar_one_or_none()  # запрос → одна задача или None
+
     # 1. найти задачу
-    task = db.query(Task).filter(Task.id == task_id).first()
     if not task:
         raise HTTPException(status_code=404, detail="Task not found")
     
@@ -73,6 +78,6 @@ def update_task(task_id: int, task_data: TaskUpdate, db: Session = Depends(get_d
     task.version += 1
 
     # 5. сохранить в БД
-    db.commit()
-    db.refresh(task)
+    await db.commit()
+    await db.refresh(task)
     return task
